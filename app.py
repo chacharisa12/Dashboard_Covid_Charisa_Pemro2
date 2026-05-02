d53177616e084caadade14954638f330479905c2
chacharisa12 <charisa.marthadps18@gmail.com>
2026-05-02 23:54:30 +0700

    tugas filter lokasi

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data import kolom, select_year, load_data, filter_data, show_data, kolom_tertentu, nama, pie_chart
+from data import kolom, select_year, load_data, filter_data, show_data, kolom_tertentu, nama, pie_chart, select_location, bar_chart1, bar_chart2, map_chart
 
 # judul dashboard
 def judul():
@@ -14,9 +14,16 @@ if menu == "Home":
     year = select_year()
     # load & filter data
     df = load_data()
-    df_filtered = filter_data(df, year)
+    # location1 = select_location1(df)
+    # df_filtered = filter_data1(df, year)
+
+    location = select_location(df)
+    df_filtered = filter_data(df, year, location)
     kolom(df_filtered)
     pie_chart(df_filtered)
+    bar_chart1(df_filtered)
+    bar_chart2(df_filtered)
+    map_chart(df_filtered)
 elif menu == "Halaman Data":
     judul()
     year = select_year()
--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from streamlit_tags import st_tags_sidebar
 
 # dfungsi 
 def load_data():
     df = pd.read_csv('covid_19_indonesia_time_series_all.csv')
+    df = df[df["Location"] != "Indonesia"]
     return df
 
 # menampilkan data dalam tabel
@@ -59,11 +61,52 @@ def nama():
 #     col2.metric(label="Total Kematian 💀", value=kematian, border=True)
 #     col3.metric(label="Total Sembuh ❤️‍🩹", value=sembuh, border=True)
 
-# filter
-def filter_data(df, year=None):
+# filter lokasi
+# def filter_data1(df, year=None, location=None):
+#     if year and year != "Semua Tahun":
+#         df = df[df["Date"].dt.year == int(year)]
+#     if location and location != "Semua Provinsi":
+#         df = df[df["Location"] == location]
+#     return df
+
+# def select_location1(df):
+#     locations = ["Semua Provinsi"] + sorted (df['Location'].unique())
+#     return st.sidebar.selectbox(
+#         "Pilih Provinsi 📍",
+#         options= locations
+#     )
+
+# FILTER LOKASI MULTISELECT
+def filter_data(df, year=None, location=None):
+
     if year != "Semua Tahun":
-        df = df[df['Date'].astype(str).str.contains(str(year))]
+        df = df[df["Date"].dt.year == int(year)]
+
+    if location:
+        df = df[df["Location"].isin(location)]
+
     return df
+
+# PILIH PROVINSI
+def select_location(df):
+    locations = sorted(df["Location"].unique())
+
+    selected = st_tags_sidebar(
+        label="Pilih Provinsi 📍",
+        text="Tekan enter setelah pilih",
+        value=[],
+        suggestions=locations,
+        maxtags=34,
+        key="1"
+    )
+
+    return selected
+
+# filter
+# def filter_data(df, year=None):
+#     if year != "Semua Tahun":
+#         df = df[df['Date'].astype(str).str.contains(str(year))]
+#     return df
 
 def select_year():
     return st.sidebar.selectbox(
@@ -88,16 +131,16 @@ def show_data(df):
     st.dataframe(df_selected.head(10))
 
 def total_case(df):
-    total_kasus = df['Total Cases'].sum()
-    return total_kasus
+    latest = df.sort_values("Date").groupby("Location", as_index=False).last()
+    return latest["Total Cases"].sum()
 
 def total_death(df):
-    total_mati = df['Total Deaths'].sum()
-    return total_mati
+    latest = df.sort_values("Date").groupby("Location", as_index=False).last()
+    return latest["Total Deaths"].sum()  
 
 def total_recovery(df):
-    total_sembuh = df['Total Recovered'].sum()
-    return total_sembuh
+    latest = df.sort_values("Date").groupby("Location", as_index=False).last()
+    return latest["Total Recovered"].sum() 
 
 def kolom(df) :
     kasus = total_case(df)
@@ -127,6 +170,83 @@ def pie_chart(df):
         title='Perbandingan Total Kematian Vs Total Kesembuhan',
         hole=0.5,
         color_discrete_sequence=['#4de89f', '#ff6459']
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+# barchart kematian terbanyak
+def bar_chart1(df):
+    df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
+
+    top5 = df_last.nlargest(5, 'Total Deaths')
+
+    fig = px.bar(
+        top5,
+        x='Location',
+        y='Total Deaths',
+        color='Total Deaths',
+        color_continuous_scale='Reds',
+        title='5 Provinsi dengan Kematian Tertinggi',
+        labels={'Total Deaths': 'Total Kematian', 'Location': 'Provinsi'}
+    )
+
+    fig.update_layout(xaxis_title='Provinsi', yaxis_title='Total Kematian', title_x=0.5)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+# bar dengan kesembuhan terbanyak
+def bar_chart2(df):
+    df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
+
+    top5 = df_last.nlargest(5, 'Total Recovered')
+
+    fig = px.bar(
+        top5,
+        x='Location',
+        y='Total Recovered',
+        color='Total Recovered',
+        color_continuous_scale='greens',
+        title='5 Provinsi dengan Kesembuhan Tertinggi',
+        labels={'Total Recovered': 'Total Kesembuhan', 'Location': 'Provinsi'}
+    )
+
+    fig.update_layout(xaxis_title='Provinsi', yaxis_title='Total Kesembuhan', title_x=0.5)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+# map chart
+def map_chart(df, year=None):
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    if year:
+        df = df[df['Date'].dt.year == year]
+
+    df_agg = df.groupby(['Location', 'Latitude', 'Longitude'], as_index=False) ['New Cases'].sum()
+    df_map = df_agg.dropna(subset=['Latitude', 'Longitude', 'New Cases'])
+
+    if df_map.empty:
+        st.info("Tidak ada data")
+        return
+    
+    fig = px.scatter_mapbox(
+        df_map,
+        lat="Latitude",
+        lon='Longitude',
+        size='New Cases',
+        color='New Cases',
+        hover_name='Location',
+        zoom=3,
+        center={'lat':-2.5, "lon": 118},
+        size_max=20,
+        opacity=0.7,
+        color_continuous_scale='OrRd',
+        title=f"Sebaran Kasus Baru Covid-19 di Indonesia ({year if year else 'Semua Tahun'})"
+    )
+
+    fig.update_layout(
+        mapbox_style="carto-positron",
+        height=600,
+        margin={"r":0, "t":0, "l":0, "b":0}
     )
 
     st.plotly_chart(fig, use_container_width=True)
